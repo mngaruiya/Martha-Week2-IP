9e7ba1f1be36e1c3269d9bde773a1cbbdfbf3b02
Martha <you@example.com>
2022-10-20 17:18:25 +0300

    Added Variables and Fuction for Male and Female

---
+++ css/script.js
@@ -0,0 +1,26 @@
+(function () {
+    let date_input = document.getElementById("date");
+    let male = document.getElementById("male");
+    let female = document.getElementById("female")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
