1d5924ab921489a2d78d8e8a0cdfe1e8a0b82d39
Martha <you@example.com>
2022-10-20 17:31:33 +0300

    Added Id to Submit button

--- css/script.js
+++ css/script.js
@@ -3,24 +3,23 @@
     let male = document.getElementById("male");
     let female = document.getElementById("female")
 
+    document.getElementById("submit").onclick = function () {
+        let gender, bdate;
+
+        bdate = date_input.value;
+
+        if (male.checked) {
+            gender = "male";
+        } else if (female.checked) {
+            gender = "female";
+        } else {
+            document.getElementById("akan-name").innerHTML = `
+            <div class="alert alert-danger" role="alert">
+                <p class="text-sm">Select options to learn your Akan name</p>
+            </div> 
+        `;
+        }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
+    }
